812be964ab0004c68f023cdde48dfe40fa8da29d
camila <camiramirezpp@gmail.com>
2018-05-25 11:14:43 -0400

    Arreglo de css: caja a textarea

--- src/cipher.js
+++ src/cipher.js
@@ -1,8 +1,29 @@
 window.cipher = {
   encode: (cifrar) => {
-    /* Acá va tu código */
-  },
+  //const abc = "abcdefghijklmnñopqrstuvwxyz"
+    //abc.charCodeArt(numero);
+    var mensaje = getElementById('mensajeUno');
+  
+    let cifrado ="";
+   
+       for(var i=0; i<mensaje.length; i++) { //el for recorrera las letras del texto a cifrar//
+        
+          if (parseInt(mensaje[i])%1 === 0) //condicionar para no ingresar numeros//
+             texto = prompt("Por favor ingrese un texto sin numeros ni espacios");
+            
+          var ubicacionCesar=(mensaje.toUpperCase().charCodeAt(i) - 65 + 33) % 26 + 65;
+    
+          var palabraCifrada= String.fromCharCode(ubicacionCesar);
+    
+      cifrado+= palabraCifrada; //acumular las letras cifradas//
+      
+      }
+        
+    return cifrado;
+    decipher(cipher());
+  }
+
+  }
   decode: (descifrar) => {
     /* Acá va tu código */
   }
-}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-/* Acá va tu código */
+window.onload = function() {
+    /* botones */
+    const btn = document.getElementById('button-number');
+    const btnCifrar = document.getElementById('botonCifrar');
+    const btnDescifrar = document.getElementById('botonDescifrar');
+    /*Mensajes ingresados*/
+    let txt = document.getElementById('mensajeUno');
+    let txtDos = document.getElementById('mensajeDos');
+
+    btnCifrar.addEventListener('click',function() {
+        cifrado.innerHTML = ('mensajeUno');
+    })
+}
