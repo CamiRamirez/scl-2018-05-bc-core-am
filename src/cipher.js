6ddb88d513d3bc2e1cf098e6152704ea8dac066d
camila <camiramirezpp@gmail.com>
2018-06-15 20:00:22 -0400

    listado de postulantes agregado

--- src/cipher.js
+++ src/cipher.js
@@ -1,58 +1,24 @@
-window.cipher = {
-  
-  encode:(stringEncode, offset) => {
-     //////////////////////////////////CODIGO PARA CIFRAR/////////////////////////////////////////////////
-    /* Variables */
-    let yourKey = parseInt(document.getElementById('key').value);
-    let messageInitial = document.getElementById('mensajeUno').value;
-    const messageUp = messageInitial.toUpperCase();
-    let letterEncode = 0;
-    let newPositionEncode = 0;
-    let newLetterEncode = "";
-    let textEncodeFinal = "";
-
-    let i = 0;
-      while (i < messageUp.length) {
-        letterEncode = messageUp.charCodeAt(i); /*Transforma char a ASCII (número de posicion de letra en el ascii)*/
-        //console.log(letterEncode);
-        newPositionEncode = (letterEncode - 65 + yourKey) % 26 + 65 ; /*nueva posicion de la letra */
-        //console.log(newPositionEncode);
-        newLetterEncode = String.fromCharCode(newPositionEncode); /* Me muestra la letra en la nueva ubicacion*/
-        //console.log(newLetterEncode);
-        textEncodeFinal = textEncodeFinal + newLetterEncode; /* Mensaje final */
-        i++; 
-    
+//cifrado
+  window.cipher = {
+    encode: (stringEncode, offset) => {
+      //Declarar las variables
+      let messageUp = stringEncode;
+      let letterEncode = "";
+      let newLetterEncode = "";
+      let textEncodeFinal = "";
+      //Métodos para cifrado
+      for (let i = 0; i < messageUp.length; i++) {
+        letterEncode = messageUp.charCodeAt(i); //me devuelve la posicion de la letra indicada
+        if (letterEncode >= 65 && letterEncode <= 90) {
+          newLetterEncode = (letterEncode - 65 + offset) % 26 + 65;
+          textEncodeFinal += String.fromCharCode(newLetterEncode);
+        } else if (letterEncode >= 32 && letterEncode <= 64) {
+          textEncodeFinal += String.fromCharCode(letterEncode);
+        } else if (letterEncode >= 97 && letterEncode <=  122) {
+          newLetterEncode = (letterEncode - 97 + offset) % 26 + 97;
+          textEncodeFinal += String.fromCharCode(newLetterEncode);
+        }
       }
       return textEncodeFinal;
-
-      //console.log('texto codificado',textEncode);
-  },
-
-  //////////////////////////////////CODIGO PARA DESCIFRAR/////////////////////////////////////////////////
-  decode: (stringDecode, offset) => {
-    /* Variables */
-    let yourKey = parseInt(document.getElementById('key').value);
-    let messageInitial2 = document.getElementById('mensajeDos').value;
-    const messageUp2 = messageInitial2.toUpperCase();
-    let letterDecode = 0;
-    let newPositionDecode = 0;
-    let newLetterDecode = "";
-    let textDecodeFinal = "";
-
-    let i = 0;
-
-      while (i < messageUp2.length) {
-        newPositionDecode=0;
-        letterDecode = messageUp2.charCodeAt(i); /*Transforma char a ASCII (posicion en el ascii)*/
-        //console.log(letterDecode);  
-        newPositionDecode = (letterDecode + 65 + yourKey) % 26 + 65; /*posicion de la letra */
-        //console.log(newPositionDecode);
-        newLetterDecode = String.fromCharCode(newPositionDecode); /*Me muestra la letra en la nueva ubicacion*/
-        //console.log(newLetterDecode);
-        textDecodeFinal = textDecodeFinal + newLetterDecode; /*Mensaje final*/
-        i++; 
-      }
-      return textDecodeFinal;
-       //console.log(textDecodefinal);
+    },
   }
-}
--- src/index.js
+++ src/index.js
@@ -1,23 +1,19 @@
 window.onload = () => {
-    /* botones */
-    const btnCifrar = document.getElementById('botonCifrar');
-    const btnDescifrar = document.getElementById('botonDescifrar');
-    
-    /* Acción al apretar boton cifrar*/
 
-    btnCifrar.addEventListener('click', () => {
-      let stringEncode = document.getElementById('mensajeUno').value;
-      let offset = parseInt(document.getElementById('key').value);
-      const textEncodeFinal = window.cipher.encode(stringEncode, offset);
-        document.getElementById('cifrado').innerHTML = textEncodeFinal;
-    });
+  /* Acción al apretar boton cifrar*/
+  document.getElementById("botonCifrar").addEventListener('click', () => {
+    const stringEncode = document.getElementById('mensajeUno').value;
+    const offset = parseInt(document.getElementById('key').value);
+    const textEncodeFinal = window.cipher.encode(stringEncode, offset);
+    document.getElementById('cifrado').innerHTML = textEncodeFinal;
+  });
 
-      /* Acción al apretar boton descifrar*/
-    btnDescifrar.addEventListener('click', () => {
-      let stringDecode = document.getElementById('mensajeDos').value;
-      let offset = parseInt(document.getElementById('key').value);
-      const textDecodeFinal = window.cipher.decode(stringDecode, offset);
-        document.getElementById('descifrado').innerHTML = textDecodeFinal;
-    });
+  /* Acción al apretar boton descifrar*/
+  document.getElementById("botonDescifrar").addEventListener('click', () => {
+    const stringDecode = document.getElementById('mensajeDos').value;
+    const offset = parseInt(document.getElementById('key').value);
+    const textDecodeFinal = window.cipher.decode(stringDecode, offset);
+    document.getElementById('descifrado').innerHTML = textDecodeFinal;
+  });
 }
 
